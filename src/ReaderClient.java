cbed796e4e12341fab71e428bb13b598195e1f73
Clamp <121687764+Clamp54@users.noreply.github.com>
2026-03-17 13:45:38 +0100

    Rename WriterClient to ReaderClient and update logic

--- src/ReaderClient.java
+++ src/ReaderClient.java
@@ -2,29 +2,24 @@ import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.util.Random;
 
-public class WriterClient {
+public class ReaderClient {
     public static void main(String[] args) {
         try {
             Registry registry = LocateRegistry.getRegistry("localhost", 1099);
             ILibrary lib = (ILibrary) registry.lookup("Library");
-            String name = "Pisarz_" + new Random().nextInt(100);
-            int bookId = 1;
+            String name = "Czytelnik_" + new Random().nextInt(100);
 
             while (true) {
-                lib.addBook(name, "Tytul_" + new Random().nextInt(1000), "2026", "Tresc");
-                
-                Thread.sleep(2000);
-                
-                if (lib.lockBook(bookId, name)) {
-                    while (!lib.isReturned(bookId)) {
-                        Thread.sleep(1000);
+                Book b = lib.findBook("Tytul");
+                if (b != null) {
+                    if (lib.borrowBook(b.id, name)) {
+                        Thread.sleep(5000);
+                        lib.returnBook(b.id, name);
                     }
-                    lib.updateBook(bookId, "Nowa Tresc", name);
                 }
                 
                 System.out.println(lib.getReport());
-                bookId++;
-                Thread.sleep(3000);
+                Thread.sleep(2000);
             }
         } catch (Exception e) {
             e.printStackTrace();
