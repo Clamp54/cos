5e485afafd7db4463661e73593715631c96cc2a3
Clamp <121687764+Clamp54@users.noreply.github.com>
2026-03-17 13:45:12 +0100

    Rename ReaderClient to WriterClient and add functionality
    Refactored ReaderClient to WriterClient with book addition and locking logic.

--- src/ReaderClient.java
+++ src/ReaderClient.java
@@ -1,19 +1,31 @@
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.util.Random;
 
-public class ReaderClient {
+public class WriterClient {
     public static void main(String[] args) {
         try {
             Registry registry = LocateRegistry.getRegistry("localhost", 1099);
             ILibrary lib = (ILibrary) registry.lookup("Library");
-            String name = "Czytelnik1";
+            String name = "Pisarz_" + new Random().nextInt(100);
+            int bookId = 1;
 
-            Book b = lib.findBook("Tytul1");
-            if (b != null) {
-                lib.borrowBook(b.id, name);
-                lib.returnBook(b.id, name);
+            while (true) {
+                lib.addBook(name, "Tytul_" + new Random().nextInt(1000), "2026", "Tresc");
+                
+                Thread.sleep(2000);
+                
+                if (lib.lockBook(bookId, name)) {
+                    while (!lib.isReturned(bookId)) {
+                        Thread.sleep(1000);
+                    }
+                    lib.updateBook(bookId, "Nowa Tresc", name);
+                }
+                
+                System.out.println(lib.getReport());
+                bookId++;
+                Thread.sleep(3000);
             }
-            System.out.println(lib.getReport());
         } catch (Exception e) {
             e.printStackTrace();
         }
